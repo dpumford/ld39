f246220b7b37f1fd1ba5120cf8e57ece207a2e4a
dpumford <dpumford@hotmail.com>
2017-07-29 10:50:13 -0500

    Adds basic grid/tile functionality.

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    public Tile[] Tiles;
+
+	// Use this for initialization
+	void Start ()
+	{
+        //TODO: Generate these based on size
+	    //GenerateTiles();
+	}
+
+    private void GenerateTiles()
+    {
+        var gridBounds = gameObject.GetComponent<Collider2D>().bounds;
+
+        for (var y = 0; y < Tiles.Length; y++)
+        {
+            for (var x = 0; x < Tiles.Length; x++)
+            {
+                //TODO: Get the math right
+            }
+        }
+    }
+
+    // Update is called once per frame
+	void Update () {
+		
+	}
+}

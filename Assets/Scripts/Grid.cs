e994127b93db820fcf1affa32adf00f24ab7d5da
dpumford <dpumford@hotmail.com>
2017-07-29 11:41:47 -0500

    Adds basic road construction/rendering.

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -1,25 +1,58 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Grid : MonoBehaviour
 {
-    public Tile[] Tiles;
+    //Assumes that the grid is square
+    public List<Tile> Tiles;
 
-	// Use this for initialization
+    public int Size
+    {
+        get { return Convert.ToInt32(Math.Sqrt(Tiles.Count)); }
+    }
+
+    // Use this for initialization
 	void Start ()
 	{
         //TODO: Generate these based on size
 	    //GenerateTiles();
 	}
 
+    public Tile GetRelative(Tile context, int dX, int dY)
+    {
+        var tileIndex = Tiles.FindIndex(tile => tile == context);
+        return Get(XFromIndex(tileIndex) + dX, YFromIndex(tileIndex) + dY);
+    }
+
+    public Tile Get(int x, int y)
+    {
+        if (x < 0 || x > Size || y < 0 || y > Size)
+        {
+            return null;
+        }
+
+        return Tiles[x + y * Size];
+    }
+
+    public int XFromIndex(int index)
+    {
+        return index % Size;
+    }
+
+    public int YFromIndex(int index)
+    {
+        return index / Size;
+    }
+
     private void GenerateTiles()
     {
         var gridBounds = gameObject.GetComponent<Collider2D>().bounds;
 
-        for (var y = 0; y < Tiles.Length; y++)
+        for (var y = 0; y < Tiles.Count; y++)
         {
-            for (var x = 0; x < Tiles.Length; x++)
+            for (var x = 0; x < Tiles.Count; x++)
             {
                 //TODO: Get the math right
             }
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -4,16 +4,58 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
-    public int X;
-    public int Y;
+    public bool Road;
+    public bool Enabled = true;
+
+    public Sprite Normal;
+    public Sprite EnabledRoad;
+    public Sprite DisabledRoad;
+
+    public float DarknessPercent = 0.95f;
+
+    private Grid _grid;
+    private SpriteRenderer _renderer;
+    private bool _hovered;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+	    _grid = GetComponentInParent<Grid>();
+	    _renderer = GetComponent<SpriteRenderer>();
 	}
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Road = true;
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            Road = false;
+        }
+
+        _hovered = true;
+    }
+
+    void OnMouseExit()
+    {
+        _hovered = false;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+	    if (Road)
+	    {
+	        _renderer.sprite = Enabled ? EnabledRoad : DisabledRoad;
+	    }
+	    else
+	    {
+	        _renderer.sprite = Normal;
+	    }
+
+	    var darkness = _hovered ? 1 : Mathf.Clamp(DarknessPercent, 0f, 1f);
+        _renderer.color = new Color(darkness, darkness, darkness);
+
 	}
 }

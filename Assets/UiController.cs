ffbb2e5b14822c18ef9f492a7f267fca54f408cb
Phantarch <rturben@uark.edu>
2017-07-30 09:21:31 -0700

    Starting to create the tab code

--- Assets/UiController.cs
+++ Assets/UiController.cs
@@ -9,15 +9,18 @@ public class UiController : MonoBehaviour {
     public Text PowerReserves;
     public Text SpaceDate;
     public Text[] CityPower;
+    public GameObject[] Tabs;
 
     public int SpaceDateWeekMillis = 10000;
-
-    private PowerSystem powerSystem;
-    private Timer elapsedWeekTimer;
 
     public int SpaceWeek = 1;
     public int SpaceMonth = 3;
     public int SpaceYear = 2197;
+
+    private PowerSystem powerSystem;
+    private Timer elapsedWeekTimer;
+    
+    private GameObject selectedTab;
 
 	// Use this for initialization
 	void Start () {
@@ -27,6 +30,10 @@ public class UiController : MonoBehaviour {
         elapsedWeekTimer.Elapsed += new ElapsedEventHandler(SpaceDateClockCalculator);
         elapsedWeekTimer.Interval = SpaceDateWeekMillis;
         elapsedWeekTimer.Enabled = true;
+        if (Tabs.Length > 0)
+        {
+            selectedTab = Tabs[0];
+        }
 	}
 	
 	// Update is called once per frame
@@ -34,6 +41,16 @@ public class UiController : MonoBehaviour {
         PowerReserves.text = powerSystem.TotalPower.ToString("N0");
         SpaceDate.text = "WEEK " + SpaceWeek + "    MONTH " + SpaceMonth + "\nYEAR " + SpaceYear;
 	}
+
+    public void SelectTab(int selectedTabIndex)
+    {
+        if (Tabs.Length-1 >= selectedTabIndex)
+        {
+            selectedTab.SetActive(false);
+            selectedTab = Tabs[selectedTabIndex];
+            selectedTab.SetActive(true);
+        }
+    }
 
     void SpaceDateClockCalculator(object source, ElapsedEventArgs e)
     {
